569adafa2960dc8fa0463b3dcf31643f109036f5
VVatashi <vvatashi@pm.me>
2025-05-19 23:45:23 +0600

    Refactor I2S

---
+++ src/i2s.h
@@ -0,0 +1,49 @@
+class I2SMasterClock
+{
+public:
+    const PIO pio;
+    const uint8_t sm;
+    const uint8_t offset;
+    const uint16_t multiplier;
+
+private:
+    uint32_t freq_hz;
+
+    I2SMasterClock(const I2SMasterClock &) = delete;
+
+public:
+    I2SMasterClock(PIO pio, uint8_t sm, uint8_t out_pin, uint32_t freq_hz, uint16_t multiplier);
+
+    void enable();
+    void disable();
+
+    void setFrequency(uint32_t freq_hz);
+    uint32_t getFrequency() const;
+
+    ~I2SMasterClock();
+};
+
+class I2SMasterInput
+{
+public:
+    const PIO pio;
+    const uint8_t sm;
+    const uint8_t offset;
+    const uint8_t frame_size;
+
+private:
+    uint32_t freq_hz;
+
+    I2SMasterInput(const I2SMasterInput &) = delete;
+
+public:
+    I2SMasterInput(PIO pio, uint8_t sm, uint8_t out_pin_base, uint8_t in_pin, uint32_t freq_hz, uint8_t frame_size);
+
+    void enable();
+    void disable();
+
+    void setFrequency(uint32_t freq_hz);
+    uint32_t getFrequency() const;
+
+    ~I2SMasterInput();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,113 +3,49 @@
 #include "hardware/clocks.h"
 #include "hardware/dma.h"
 #include "hardware/pio.h"
-#include "i2s.pio.h"
+#include "i2s.h"
 
-gpio_function_t get_gpio_function(PIO pio)
+static const uint16_t rx_buffer_size = 8192;
+
+alignas(4) static uint32_t rx_buffer_0[rx_buffer_size];
+alignas(4) static uint32_t rx_buffer_1[rx_buffer_size];
+
+static volatile bool rx_buffer_0_ready = false;
+static volatile bool rx_buffer_1_ready = false;
+
+static uint8_t dma_channel = 0;
+
+static void __isr dma_handler()
 {
-    if (pio == pio0)
-    {
-        return GPIO_FUNC_PIO0;
-    }
+    static uint8_t current_buffer = 0;
 
-    if (pio == pio1)
+    if (dma_channel_get_irq0_status(dma_channel))
     {
-        return GPIO_FUNC_PIO1;
-    }
+        dma_channel_acknowledge_irq0(dma_channel);
 
-    // RP2350 has 3 PIO blocks
-#ifdef GPIO_FUNC_PIO2
-    if (pio == pio2)
-    {
-        return GPIO_FUNC_PIO2;
-    }
-#endif
+        if (current_buffer == 0)
+        {
+            current_buffer = 1;
+            rx_buffer_0_ready = true;
+            dma_channel_set_write_addr(dma_channel, rx_buffer_1, false);
+        }
+        else
+        {
+            current_buffer = 0;
+            rx_buffer_1_ready = true;
+            dma_channel_set_write_addr(dma_channel, rx_buffer_0, false);
+        }
 
-    return GPIO_FUNC_NULL;
+        dma_channel_set_trans_count(dma_channel, rx_buffer_size, true);
+    }
 }
 
-uint init_i2s_master_clock(PIO pio, uint sm, uint offset, uint out_pin, float freq_hz)
-{
-    pio_sm_config config = i2s_master_clock_program_get_default_config(offset);
-
-    sm_config_set_sideset_pins(&config, out_pin);
-    sm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (2 * freq_hz));
-
-    pio_gpio_init(pio, out_pin);
-
-    pio_sm_set_consecutive_pindirs(pio, sm, out_pin, 1, true);
-
-    pio_sm_init(pio, sm, offset, &config);
-
-    return sm;
-}
-
-uint init_i2s_master_input(PIO pio, uint sm, uint offset, uint out_pin_base, uint in_pin, float freq_hz)
-{
-    pio_sm_config config = i2s_master_input_program_get_default_config(offset);
-
-    sm_config_set_sideset_pins(&config, out_pin_base);
-    sm_config_set_in_pins(&config, in_pin);
-
-    sm_config_set_in_shift(&config, false, true, 32);
-    sm_config_set_fifo_join(&config, PIO_FIFO_JOIN_RX);
-    sm_config_set_clkdiv(&config, clock_get_hz(clk_sys) / (2 * freq_hz));
-
-    pio_gpio_init(pio, out_pin_base);
-    pio_gpio_init(pio, out_pin_base + 1);
-    pio_gpio_init(pio, in_pin);
-
-    pio_sm_set_consecutive_pindirs(pio, sm, out_pin_base, 2, true);
-    pio_sm_set_consecutive_pindirs(pio, sm, in_pin, 1, false);
-    gpio_set_function(in_pin, get_gpio_function(pio));
-
-    pio_sm_init(pio, sm, offset, &config);
-    pio_sm_exec(pio, sm, pio_encode_jmp(offset + i2s_master_input_offset_entry_point));
-
-    return sm;
-}
-
-const uint16_t rx_buffer_size = 8000;
-
-uint32_t rx_buffer_0[rx_buffer_size];
-uint32_t rx_buffer_1[rx_buffer_size];
-
-uint8_t current_buffer = 0;
-
-uint8_t dma_channel = 0;
-
-void __isr dma_handler()
-{
-    dma_hw->ints0 = 1u << dma_channel;
-
-    if (current_buffer == 0)
-    {
-        dma_channel_set_write_addr(0, rx_buffer_1, false);
-        current_buffer = 1;
-    }
-    else
-    {
-        dma_channel_set_write_addr(0, rx_buffer_0, false);
-        current_buffer = 0;
-    }
-
-    dma_channel_set_trans_count(0, rx_buffer_size, true);
-
-    for (int i = 0; i < 8; i++)
-    {
-        uint32_t sample = (current_buffer == 0 ? rx_buffer_1[i] : rx_buffer_0[i]) << 1;
-        int32_t signed_sample = ((int32_t)sample) >> 8;
-        printf("%08X ", signed_sample);
-    }
-
-    printf("\n");
-}
-
-void setup_rx_dma(PIO pio, uint sm)
+static void setup_rx_dma(PIO pio, uint8_t sm)
 {
     dma_channel = dma_claim_unused_channel(true);
 
     dma_channel_config config = dma_channel_get_default_config(dma_channel);
+    channel_config_set_transfer_data_size(&config, DMA_SIZE_32);
     channel_config_set_read_increment(&config, false);
     channel_config_set_write_increment(&config, true);
     channel_config_set_dreq(&config, pio_get_dreq(pio, sm, false));
@@ -135,25 +71,49 @@ int main()
 
     const PIO pio = pio0;
 
-    const uint SCK_PIN = 16;
-    const uint BCK_PIN = 17;
-    const uint LRCK_PIN = 18;
-    const uint DIN_PIN = 19;
+    const uint8_t SCK_PIN = 16;
+    const uint8_t BCK_PIN = 17;
+    const uint8_t LRCK_PIN = 18;
+    const uint8_t DIN_PIN = 19;
 
-    const uint32_t sample_freq = 96000;
+    const uint32_t sample_freq = 8000;
 
-    uint i2s_clock_offset = pio_add_program(pio, &i2s_master_clock_program);
-    uint i2s_clock_sm = init_i2s_master_clock(pio, pio_claim_unused_sm(pio, true), i2s_clock_offset, SCK_PIN, 256 * sample_freq);
+    I2SMasterClock clock(pio, pio_claim_unused_sm(pio, true), SCK_PIN, sample_freq, 256);
+    I2SMasterInput input(pio, pio_claim_unused_sm(pio, true), BCK_PIN, DIN_PIN, sample_freq, 32);
 
-    uint i2s_input_offset = pio_add_program(pio, &i2s_master_input_program);
-    uint i2s_input_sm = init_i2s_master_input(pio, pio_claim_unused_sm(pio, true), i2s_input_offset, BCK_PIN, DIN_PIN, 64 * sample_freq);
-    setup_rx_dma(pio, i2s_input_sm);
+    setup_rx_dma(pio, input.sm);
 
-    pio_sm_set_enabled(pio, i2s_clock_sm, true);
-    pio_sm_set_enabled(pio, i2s_input_sm, true);
+    clock.enable();
+    input.enable();
 
     while (true)
     {
-        tight_loop_contents();
+        if (rx_buffer_0_ready)
+        {
+            rx_buffer_0_ready = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+                uint32_t sample = (rx_buffer_0[i]) << 1;
+                int32_t signed_sample = ((int32_t)sample) >> 8;
+                printf("%08X ", signed_sample);
+            }
+
+            printf("\n");
+        }
+
+        if (rx_buffer_1_ready)
+        {
+            rx_buffer_1_ready = false;
+
+            for (int i = 0; i < 8; i++)
+            {
+                uint32_t sample = (rx_buffer_1[i]) << 1;
+                int32_t signed_sample = ((int32_t)sample) >> 8;
+                printf("%08X ", signed_sample);
+            }
+
+            printf("\n");
+        }
     }
 }
